e18881805efcdddf0b3ba79710566fd90257fe34
Jesusebz <48937476+jesusebzcp@users.noreply.github.com>
2024-06-18 16:29:26 -0500

    update confog

--- app.config.ts
+++ app.config.ts
@@ -2,52 +2,47 @@ import { ExpoConfig, ConfigContext } from "expo/config";
 
 const IS_QA = process.env.APP_VARIANT === "qa";
 
-const defaultConfig: { expo: ExpoConfig } = {
-  expo: {
-    name: "CodemagicSample",
-    slug: "CodemagicSample",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/images/icon.png",
-    scheme: "myapp",
-    userInterfaceStyle: "automatic",
-    splash: {
-      image: "./assets/images/splash.png",
-      resizeMode: "contain",
+const defaultConfig: ExpoConfig = {
+  name: "CodemagicSample",
+  slug: "CodemagicSample",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "myapp",
+  userInterfaceStyle: "automatic",
+  splash: {
+    image: "./assets/images/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: IS_QA ? "com.finbankcoapp.qa" : "com.finbankcoapp",
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
       backgroundColor: "#ffffff",
     },
-    ios: {
-      supportsTablet: true,
-      bundleIdentifier: IS_QA ? "com.finbankcoapp.qa" : "com.finbankcoapp",
+    package: IS_QA ? "com.finbankcoapp.qa" : "com.finbankcoapp",
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: ["expo-router"],
+  experiments: {
+    typedRoutes: true,
+  },
+  extra: {
+    router: {
+      origin: false,
     },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
-        backgroundColor: "#ffffff",
-      },
-      package: IS_QA ? "com.finbankcoapp.qa" : "com.finbankcoapp",
-    },
-    web: {
-      bundler: "metro",
-      output: "static",
-      favicon: "./assets/images/favicon.png",
-    },
-    plugins: ["expo-router"],
-    experiments: {
-      typedRoutes: true,
-    },
-    extra: {
-      router: {
-        origin: false,
-      },
-      eas: {
-        projectId: "292c2163-006c-48ef-8c4a-30c452427a04",
-      },
+    eas: {
+      projectId: "292c2163-006c-48ef-8c4a-30c452427a04",
     },
   },
 };
 
-export default ({ config }: ConfigContext): ExpoConfig => ({
-  ...config,
-  ...defaultConfig,
-});
+export default ({ config }: ConfigContext): ExpoConfig => defaultConfig;
